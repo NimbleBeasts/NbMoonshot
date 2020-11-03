801f828b6a7d0251c97dd716dc2c272fdda8c5fc
Allen Davis <allendaviskoonan@gmail.com>
2020-11-03 21:48:00 +0530

    Added sprinting. Can specify both sprint speed, sprint acceleration as well as normal speed and normal acceleration Hole Shift or X to sprint, change this in the input map if you want

--- Src/Player/Player.gd
+++ Src/Player/Player.gd
@@ -1,23 +1,32 @@
 class_name Player
 extends KinematicBody2D
 
+# movement variables
+export var normal_speed: int = 100
+export var normal_acceleration: int = 600
+export var sprint_speed: int = 400
+export var sprint_acceleration: int = 2500
+
 var direction: Vector2
 var velocity: Vector2
-
-export var max_speed: int = 100
-export var acceleration: int = 600
-export var friction: int = 600
+var speed: int = normal_speed
+var acceleration: int = normal_acceleration
 
 
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("sprint"):
+		speed = sprint_speed
+		acceleration = sprint_acceleration
+	else:
+		speed = normal_speed
+		acceleration = normal_acceleration
+			
+		
 func _physics_process(delta: float) -> void:
 	# movement code
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction = direction.normalized()
 	
-	# moves with acceleration or friction depending on whether player has stopped
-	if direction != Vector2.ZERO:
-		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	velocity = velocity.move_toward(direction * speed, acceleration * delta)
 		
 	velocity = move_and_slide(velocity)
