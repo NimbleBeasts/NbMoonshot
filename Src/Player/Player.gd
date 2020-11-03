db545fb4d0ef1146ac0e624b4721fbcb88604ddb
Allen Davis <allendaviskoonan@gmail.com>
2020-11-03 21:31:03 +0530

    Simple movement code for player with exported variables. Acceleration and friction are multiplied by delta so those values are bigger than max_speed which is not multiplied by delta

--- Src/Player/Player.gd
+++ Src/Player/Player.gd
@@ -1,16 +1,23 @@
+class_name Player
 extends KinematicBody2D
 
+var direction: Vector2
+var velocity: Vector2
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export var max_speed: int = 100
+export var acceleration: int = 600
+export var friction: int = 600
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta: float) -> void:
+	# movement code
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction = direction.normalized()
+	
+	# moves with acceleration or friction depending on whether player has stopped
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		
+	velocity = move_and_slide(velocity)
