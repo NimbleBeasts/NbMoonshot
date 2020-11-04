111b9d4353a23cb7e98428d6da68304f55214d77
Allen Davis <allendaviskoonan@gmail.com>
2020-11-04 17:18:11 +0530

    Made thinArea to go up and down

---
+++ Src/Minigames/TestMinigame.gd
@@ -0,0 +1,6 @@
+class_name Minigame
+extends Node2D
+
+onready var tween := $Tween
+export (Types.Minigames) var type
+
---
+++ Src/ThinArea/ThinArea.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var player_entered: bool = false
+
+
+func _ready() -> void:
+	connect("area_entered", self, "_on_area_entered")
+	connect("area_exited", self, "_on_area_exited")
+
+
+func _process(delta: float) -> void:
+	if player_entered:
+		if Input.is_action_just_pressed("travel_up"):
+			Events.emit_signal("player_travel", $DestinationUp.global_position)
+		elif Input.is_action_just_pressed("travel_down"):
+			Events.emit_signal("player_travel", $DestinationDown.global_position)
+		
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("PlayerTravelTrigger"):
+		player_entered = true
+		
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.is_in_group("PlayerTravelTrigger"):
+		player_entered = false
