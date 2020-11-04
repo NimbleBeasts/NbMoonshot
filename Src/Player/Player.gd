b4cb294064797cfe08f5fa5b1d50d9bb2d71caed
Allen Davis <allendaviskoonan@gmail.com>
2020-11-04 12:00:48 +0530

    Added wall dodging. Press W to wall dodge or change it in the InputMap

--- Src/Player/Player.gd
+++ Src/Player/Player.gd
@@ -12,17 +12,17 @@ var velocity: Vector2
 var speed: int = normal_speed
 var acceleration: int = normal_acceleration
 
-var visible_level: int = Types.VisibleLevels.Dark #  Use Types.VisibleLevels enum for this
+#  Use Types.LightLevels enum for both of these. Light level is in which light the player is in
+# and visible_level is actual visibility of player to guards and camera with wall dodging and other benefits
+var light_level: int = Types.LightLevels.Dark
+var visible_level: int = light_level
 
 func _init() -> void:
 	Global.player = self
 
 
-func _ready() -> void:
-	check_if_dark()
-	
-
 func _process(delta: float) -> void:
+	# changed between speeds depending on whether sprinting or not
 	if Input.is_action_pressed("sprint"):
 		speed = sprint_speed
 		acceleration = sprint_acceleration
@@ -31,8 +31,14 @@ func _process(delta: float) -> void:
 		acceleration = normal_acceleration
 	
 	check_if_dark()
-		
-		
+	
+	# wall dodging
+	if Input.is_action_pressed("wall_dodge"):
+		visible_level = light_level - 1
+	elif Input.is_action_just_released("wall_dodge"):
+		visible_level = light_level
+	
+	
 func _physics_process(delta: float) -> void:
 	# movement code
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -43,22 +49,31 @@ func _physics_process(delta: float) -> void:
 
 
 func check_if_dark() -> void:
-	# if there are no overlapping areas, just set visible_level to dark
+	# if there are no overlapping areas, just set light_level to dark
 	if $PlayerArea.get_overlapping_areas() == []:
-		visible_level = Types.VisibleLevels.Dark
-		return 		
+		set_light_level(Types.LightLevels.Dark)
+		return 
 	
 	# checks all area to see if there are no barely visible and no full light areas 
 	for area in $PlayerArea.get_overlapping_areas():
 		if (not area.is_in_group("BarelyVisible")) and (not area.is_in_group("FullLight")):
-			visible_level = Types.VisibleLevels.Dark
+			set_light_level(Types.LightLevels.Dark)
 
 
 func _on_PlayerArea_area_entered(area: Area2D) -> void:
 	if area.is_in_group("FullLight"):
-		visible_level = Types.VisibleLevels.FullLight
+		set_light_level(Types.LightLevels.FullLight)
 	elif area.is_in_group("BarelyVisible"):
-		visible_level = Types.VisibleLevels.BarelyVisible
-
-
+		set_light_level(Types.LightLevels.BarelyVisible)
 	
+
+# use this function to set light_level instead of directly changing it
+func set_light_level(value: int) -> void:
+	if not light_level == value:
+		light_level = value
+		# updates visible level when updating light_level
+		# this is why a custom setter function is needed, may forgot to set visible level and
+		# will fuk everything up
+		visible_level = value
+	
+
--- Src/_Autoloads/Types.gd
+++ Src/_Autoloads/Types.gd
@@ -2,4 +2,4 @@ extends Node
 
 enum Direction { Top, Right, Down, Left }
 enum GameStates {Menu, Game, Settings} 
-enum VisibleLevels {Dark, BarelyVisible, FullLight}
+enum LightLevels {Dark, BarelyVisible, FullLight}
