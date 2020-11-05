c1ea3ce2bb12fe7ec576ad71bf3e7e29279f2393
Allen Davis <allendaviskoonan@gmail.com>
2020-11-05 11:32:07 +0530

    Guards now can do sure detection if player stand in guard fov and not taken out in time. Use the player_detected signal in Events to check if it is possible or sure detection

--- Src/Guard/Guard.gd
+++ Src/Guard/Guard.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 export var speed: int = 50
 export var direction_change_time: float = 2
 export var starting_direction: Vector2
+export var time_to_sure_direction: float = 1.5
 
 var velocity: Vector2
 var direction: Vector2
@@ -15,14 +16,19 @@ func _ready() -> void:
 	$DirectionChangeTimer.wait_time = direction_change_time
 	$DirectionChangeTimer.start()
 	direction = starting_direction 
-	
+	$SureDetectionTimer.wait_time = time_to_sure_direction
 
 func _process(delta: float) -> void:
 	velocity = direction * speed
 	velocity = move_and_slide(velocity)
+	
+	#Player detection
 	if player_detected:
 		direction = Vector2(0,0)
-		
+		# Starts a timer to emit sure detection
+		if $SureDetectionTimer.is_stopped():
+			$SureDetectionTimer.start()
+			
 
 func _on_DirectionChangeTimer_timeout():
 	change_direction()
@@ -38,3 +44,7 @@ func _on_LineOfSight_area_entered(area: Area2D) -> void:
 	if area.is_in_group("PlayerArea"):
 		Events.emit_signal("player_detected", Types.DetectionLevels.Possible)
 		player_detected = true
+
+
+func _on_SureDetectionTimer_timeout():
+	Events.emit_signal("player_detected", Types.DetectionLevels.Sure)
