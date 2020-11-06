29e32a59ee16f77976ee44d07f0baa3e318cea13
Allen Davis <allendaviskoonan@gmail.com>
2020-11-06 21:58:47 +0530

    Changed the minigame system up a bit so that doors can now have exported variables for info of their minigame type it doesn't matter what an exported variable is if it doesn't correspond to the type (which is also an exported variable)

--- Src/Minigames/MinigameDoor.gd
+++ Src/Minigames/MinigameDoor.gd
@@ -2,6 +2,7 @@ class_name MinigameDoor
 extends Area2D
 
 export (Types.Minigames) var type
+export (Array, int) var lock_code: Array
 
 var player_entered: bool = false
 var minigame: Minigame
@@ -14,14 +15,7 @@ onready var game_manager := get_node("/root/GameManager")
 func _ready() -> void:
 	connect("area_entered", self, "_on_area_entered")
 	connect("area_exited", self, "_on_area_exited")
-	# sets minigame scene checking type
 	
-	match type:
-		Types.Minigames.Test:
-			minigame_scene = load("res://Src/Minigames/TestMinigame.tscn")
-		Types.Minigames.Keypad:
-			minigame_scene = load("res://Src/Minigames/KeypadMinigame/KeypadMinigame.tscn")
-
 
 func _process(delta: float) -> void:
 	if player_entered: # if player is near
@@ -33,8 +27,8 @@ func _process(delta: float) -> void:
 	
 	
 func create_minigame() -> Minigame:
-	# Spawning minigame scene
-	var minigame_instance: Minigame = minigame_scene.instance()
+	var minigame_instance: Minigame = get_minigame_instance_by_type()
+
 	game_manager.levelNode.get_node("HUD").add_child(minigame_instance)
 	
 	# sets position to bottom center of the screen
@@ -43,6 +37,18 @@ func create_minigame() -> Minigame:
 	minigame_instance.owner_obj = self # sets owner obj to self so it has a reference to this node
 	
 	return minigame_instance
+
+# this function checks type and returns proper instance and also sets variables to exported variables here
+func get_minigame_instance_by_type() -> Minigame:
+	var result: Minigame
+	match type:
+		Types.Minigames.Test:
+			result = load("res://Src/Minigames/TestMinigame.tscn").instance()
+		Types.Minigames.Keypad:
+			result = load("res://Src/Minigames/KeypadMinigame/KeypadMinigame.tscn").instance()
+			result.goal_array = lock_code
+	
+	return result
 	
 	
 func _on_area_entered(area: Area2D) -> void:
