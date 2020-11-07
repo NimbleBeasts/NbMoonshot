d1ea646be209c18b017581b3c4aae58fc5190100
Allen Davis <allendaviskoonan@gmail.com>
2020-11-07 12:31:43 +0530

    Made new WireCutMinigame scene

--- Src/_Autoloads/Types.gd
+++ Src/_Autoloads/Types.gd
@@ -5,7 +5,7 @@ enum GameStates {Menu, Game, Settings}
 enum LightLevels {Dark, BarelyVisible, FullLight}
 enum AudioLevels {LoudNoise = 0, SmallNoise = 1, Silent = 2}
 enum PlayerStates {Normal, WallDodge, Duck}
-enum Minigames{Test, Keypad}
+enum Minigames{Test, Keypad, WireCut}
 enum MinigameResults{Failed, Succeeded, Doing}
 enum DetectionLevels{Possible, Sure}
 enum GuardStates {Wander, Suspect, PlayerDetected}
