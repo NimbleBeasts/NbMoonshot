f2c8976b4943d2448148687b2350ec312591cc13
Allen Davis <allendaviskoonan@gmail.com>
2020-11-08 22:06:26 +0530

    Something of a dialogue system for the boss NPC

--- Src/NPCs/Boss.gd
+++ Src/NPCs/Boss.gd
@@ -1,5 +1,10 @@
 extends NPC
 
+var interacted_counter: int = 1
+
 # function gets called when you get close to this and press "interact" in input map
 func _on_player_interacted() -> void:
-	print("Player has interacted with Boss")
+	var dialogue := load_dialogue()
+	if not dialogue.empty() and dialogue.has(str(interacted_counter)):
+		print(dialogue[str(interacted_counter)]["text"])
+		interacted_counter += 1
--- Src/NPCs/NPCs.gd
+++ Src/NPCs/NPCs.gd
@@ -2,7 +2,9 @@ class_name NPC
 extends Area2D
 
 signal player_interacted
+export (String, FILE) var dialogue_path: String
 var player_entered: bool = false
+
 
 func _ready() -> void:
 	connect("player_interacted", self, "_on_player_interacted")
@@ -16,6 +18,13 @@ func _process(delta: float) -> void:
 		Events.emit_signal("interacted_with_npc", self)
 		emit_signal("player_interacted")
 		
+# function for loading dialogues
+func load_dialogue() -> Dictionary:
+	var file := File.new()
+	file.open(dialogue_path, file.READ)
+	var dialogue = parse_json(file.get_as_text())
+	return dialogue
+
 
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
