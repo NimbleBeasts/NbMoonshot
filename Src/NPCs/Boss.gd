73d39a22d5aca138cfe715de1e524db279bf215e
Allen Davis <allendaviskoonan@gmail.com>
2020-11-08 22:46:55 +0530

    Final commit 4 today

--- Src/NPCs/Boss.gd
+++ Src/NPCs/Boss.gd
@@ -2,8 +2,8 @@ extends NPC
 
 var interacted_counter: int = 1
 
-# function gets called when you get close to this and press "interact" in input map
-func _on_player_interacted() -> void:
+
+func interact() -> void:
 	var dialogue := load_dialogue()
 	if not dialogue.empty() and dialogue.has(str(interacted_counter)):
 		print(dialogue[str(interacted_counter)]["text"])
--- Src/NPCs/NPCs.gd
+++ Src/NPCs/NPCs.gd
@@ -1,13 +1,11 @@
 class_name NPC
 extends Area2D
 
-signal player_interacted
 export (String, FILE) var dialogue_path: String
 var player_entered: bool = false
 
 
 func _ready() -> void:
-	connect("player_interacted", self, "_on_player_interacted")
 	connect("body_entered", self, "_on_body_entered")
 	connect("body_exited", self, "_on_body_exited")
 
@@ -16,15 +14,20 @@ func _process(delta: float) -> void:
 	# _process will only get called if player is in the area so we don't need to put that additional check
 	if Input.is_action_just_pressed("interact"):
 		Events.emit_signal("interacted_with_npc", self)
-		emit_signal("player_interacted")
-		
+		interact()
+
+
 # function for loading dialogues
 func load_dialogue() -> Dictionary:
+	if dialogue_path == "":
+		return {}
 	var file := File.new()
 	file.open(dialogue_path, file.READ)
 	var dialogue = parse_json(file.get_as_text())
 	return dialogue
 
+func interact():
+	pass
 
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
