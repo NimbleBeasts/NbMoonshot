6dc9af907c228ad031580e2bd4f0abaac41b6312
mago <markus.schneider84@gmail.com>
2020-11-09 13:04:27 +0100

    added camera animations, not yet fully implemented. removed code from light

--- Src/Camera/Camera.gd
+++ Src/Camera/Camera.gd
@@ -3,6 +3,24 @@ extends Node2D
 var state: int = Types.CameraStates.Normal
 var player_in_fov: bool = false
 
+enum CamDirectionType {Left, Right}
+
+export(CamDirectionType) var camDirection = CamDirectionType.Left
+export(bool) var isFixedCam = false
+
+
+var currentDirection = camDirection
+
+
+func _ready():
+	if camDirection == CamDirectionType.Left:
+		animation("idle_left")
+	else:
+		animation("idle_right")
+	
+	if not isFixedCam:
+		$RotationTimer.start()
+
 
 func _process(_delta: float) -> void:
 	if player_in_fov:
@@ -43,3 +61,31 @@ func set_state(new_state) -> void:
 					$SureDetectionTimer.start()
 				Events.emit_signal("player_detected", Types.DetectionLevels.Possible)
 				
+
+func animation(anim):
+	if $AnimationPlayer.current_animation != anim:
+		$AnimationPlayer.play(anim)
+
+
+func _on_RotationTimer_timeout():
+	#TODO:
+	print("disable camera fov")
+	
+	if currentDirection == CamDirectionType.Left:
+		$AnimationPlayer.play("rotation")
+	else:
+		$AnimationPlayer.play_backwards("rotation")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "rotation":
+		# Set new cam direction
+		if currentDirection == CamDirectionType.Left:
+			camDirection = CamDirectionType.Right
+			$AnimationPlayer.play("idle_right")
+		else:
+			camDirection = CamDirectionType.Left
+			$AnimationPlayer.play("idle_left")
+		# Todo
+		print("update camera fov and enable it")
+		
--- Src/Light/Light.gd
+++ Src/Light/Light.gd
@@ -5,29 +5,31 @@ var can_check_for_player
 
 # hmmmmmm this is shit :<
 
-func _draw():
-	for point in debugPoints:
-		draw_circle(point, 6, Color(255,150,0))
-
-
-func raycastToPlayer(targetPos):
-	targetPos -= Vector2(0, 16)
-	debugPoints = [Vector2(0, 0), targetPos]
-	
-	var space_state = get_world_2d().direct_space_state
-	var result = space_state.intersect_ray(Vector2(0, 0), targetPos)
-	
-	if result:
-		debugPoints.append(result.position)
-	print(result)
-	print(debugPoints)
-	update()
+#func _draw():
+#	for point in debugPoints:
+#		draw_circle(point, 6, Color(255,150,0))
+#
+#
+#func raycastToPlayer(targetPos):
+#	targetPos -= Vector2(0, 16)
+#	debugPoints = [Vector2(0, 0), targetPos]
+#
+#	var space_state = get_world_2d().direct_space_state
+#	var result = space_state.intersect_ray(Vector2(0, 0), targetPos)
+#
+#	if result:
+#		debugPoints.append(result.position)
+#	print(result)
+#	print(debugPoints)
+#	update()
 
 func _on_FullLight_body_entered(body):
 	if body.is_in_group("Player"):
-		raycastToPlayer(body.position - global_position)
+		return
+		#raycastToPlayer(body.position - global_position)
 
 
 func _on_BarelyVisible_body_entered(body):
 	if body.is_in_group("Player"):
-		raycastToPlayer(body.position - global_position)
+		return
+		#raycastToPlayer(body.position - global_position)
