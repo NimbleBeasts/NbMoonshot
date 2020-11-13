d996f241059e867907fa9e8a1c6274dca1b1e7be
mago <markus.schneider84@gmail.com>
2020-11-13 14:04:19 +0100

    added tresor

--- Src/Tresor/Tresor.gd
+++ Src/Tresor/Tresor.gd
@@ -1,1 +1,8 @@
-extends MinigameSpawner
+extends Node2D 
+
+func _ready():
+	$KeypadMinigameSpawner.connect("minigame_succeeded", self, "openTresor")
+	$KeypadMinigameSpawner.lock_code = 1337
+	
+func openTresor():
+	$Sprite.frame = 1
