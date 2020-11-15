581a3394abbd685b42cb2e5ff485f8e144864965
mago <markus.schneider84@gmail.com>
2020-11-15 09:32:55 +0100

    started training zone

---
+++ Assets/Camera/CamTraining.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+var player_in_fov = null
+
+
+func _ready():
+	set_process(false)
+
+func _process(_delta: float) -> void:
+	if player_in_fov:
+		match Global.player.visible_level:
+			Types.LightLevels.FullLight:
+				$AnimationPlayer.play("detected")
+			Types.LightLevels.BarelyVisible:
+				$AnimationPlayer.play("suspect")
+			Types.LightLevels.Dark:
+				$AnimationPlayer.play("idle")
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		player_in_fov = body
+		set_process(true)
+
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("Player"):
+		player_in_fov = null
+		$AnimationPlayer.play("idle")
+		set_process(false)
