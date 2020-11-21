d19e26e0bac2a33ca3e8dc0a82bf56f6b7167096
Allen Davis <allendaviskoonan@gmail.com>
2020-11-21 17:16:33 +0530

    to pull

--- Src/Minigames/PhotoMinigame/PhotoMinigame.gd
+++ Src/Minigames/PhotoMinigame/PhotoMinigame.gd
@@ -2,6 +2,9 @@ extends Minigame
 
 export (Texture) var targetTexture: Texture
 export var countDownTime: int
+export var pictureOffsetToCamera: Vector2
+export var pictureTravelLength: float = 50
+export var pictureTravelDuration: float = 2.0
 
 var targetEnteredCamera: bool = false
 var timeLeftString: String 
@@ -13,9 +16,12 @@ var cameraMoveSpeed
 onready var timeSprite1: Sprite = $CountdownSprites/Time1
 onready var timeSprite2: Sprite = $CountdownSprites/Time2
 onready var countDownTimer: Timer = $CountdownTimer
+onready var pictureTaken: Sprite = $PictureTaken
+onready var pictureTakenTween: Tween = $PictureTakenTween
 
 
 func _ready() -> void:
+	pictureTakenTween.connect("tween_all_completed", self, "onPictureTweenAllCompleted")
 	$Target/Sprite.texture = targetTexture
 	$Camera.moveSpeed = cameraMoveSpeed
 	$Target/CollisionShape2D.shape.extents = targetColliderExtents
@@ -33,9 +39,14 @@ func _process(delta: float) -> void:
 		
 	if targetEnteredCamera:
 		if Input.is_action_just_pressed("interact"):
-			set_result(Types.MinigameResults.Succeeded)
-			close()
-			
+			pictureTaken.texture = targetTexture
+			pictureTaken.scale = $Target/Sprite.scale
+			pictureTaken.position = $Camera.position + pictureOffsetToCamera
+			pictureTakenTween.interpolate_property(pictureTaken, "position:y", pictureTaken.position.y, pictureTaken.position.y - pictureTravelLength, pictureTravelDuration, Tween.TRANS_LINEAR)
+			pictureTakenTween.start()
+			canCloseMinigame = false
+			$Camera.canMove = false
+
 func onCameraAreaEntered(area: Area2D) -> void:
 	if area.is_in_group("Target"):
 		targetEnteredCamera = true
@@ -57,3 +68,9 @@ func setTimeLeftString(value: String) -> void:
 		print("failed photo minigame")
 		set_result(Types.MinigameResults.Failed)
 		close()
+
+
+func onPictureTweenAllCompleted() -> void:
+	canCloseMinigame = true
+	set_result(Types.MinigameResults.Succeeded)
+	close()
