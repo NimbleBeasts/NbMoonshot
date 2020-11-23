9d3b5524959e9cd7ccc921505cd565b600074ced
kodkuce <kodkuce@mail.ru>
2020-11-23 23:26:34 +0100

    Staircase lockpick

--- Src/Staircase/Staircase.gd
+++ Src/Staircase/Staircase.gd
@@ -5,10 +5,16 @@ enum DoorLockType {open, lockedLevel1, lockedLevel2, locked}
 export(DoorLockType) var lockLevel = DoorLockType.open
 var open: bool = false
 
+export var door_name : String = ""
+
 func _ready():
 	animPlayer.connect("animation_finished", self, "onAnimationFinished")
-	lockLevel = DoorLockType.locked
+	#lockLevel = DoorLockType.locked
 	$Sprite.frame = 0
+	
+	if door_name != "":
+		Events.connect("door_change_status", self, "_on_door_change_status")
+	
 	# if lockLevel == DoorLockType.open:
 	# 	$Sprite.frame = 2
 	# else:
@@ -17,6 +23,14 @@ func _ready():
 
 # overriden
 func interact() -> void:
+	if lockLevel == DoorLockType.lockedLevel1:
+		$LockpickSmallMinigameSpawner.run_minigame(door_name, 1, true)
+		return
+	if lockLevel == DoorLockType.lockedLevel2:
+		$LockpickSmallMinigameSpawner.run_minigame(door_name, 2, true)
+		return
+	
+	
 	# teleports to connected door
 	if connected_door and not animPlayer.is_playing():
 		Events.emit_signal("block_player_movement")
@@ -39,4 +53,8 @@ func onAnimationFinished(animName: String) -> void:
 		print("exit door")
 		open = false
 		
-
+func _on_door_change_status(_door_name, _lock_type, _run_anim):
+	if door_name == _door_name:
+		lockLevel = _lock_type
+		if _run_anim:
+			interact()
