30273828269c5614149c76be9727b955884b9173
kodkuce <kodkuce@mail.ru>
2020-11-25 00:37:04 +0100

    cam colors init

--- Src/Camera/Camera.gd
+++ Src/Camera/Camera.gd
@@ -10,7 +10,7 @@ export(bool) var isFixedCam = false
 
 
 var currentDirection = camDirection
-
+var atm_rotating = false
 
 func _ready():
 	if camDirection == CamDirectionType.Left:
@@ -26,16 +26,20 @@ func _ready():
 
 
 func _process(_delta: float) -> void:
-	if player_in_fov:
-		match Global.player.visible_level:
-			Types.LightLevels.FullLight:
-				set_state(Types.CameraStates.PlayerDetected)
-				player_in_fov = false
-			Types.LightLevels.BarelyVisible:
-				set_state(Types.CameraStates.Suspect)
-			Types.LightLevels.Dark:
-				set_state(Types.CameraStates.Normal)
-
+	#dident know if something else is connected
+	#to Types.CameraStates so i just added 
+	#atm_rotating check insted Types.CameraState.Rotating
+	if not atm_rotating:
+		if player_in_fov:	
+			#print("am in fov")
+			match Global.player.visible_level:
+				Types.LightLevels.FullLight:
+					set_state(Types.CameraStates.PlayerDetected)
+					player_in_fov = false
+				Types.LightLevels.BarelyVisible:
+					set_state(Types.CameraStates.Suspect)
+				Types.LightLevels.Dark:
+					set_state(Types.CameraStates.Normal)
 
 func _on_FOV_area_entered(area: Area2D) -> void:
 	if area.is_in_group("PlayerArea"):
@@ -45,11 +49,29 @@ func _on_FOV_area_entered(area: Area2D) -> void:
 func _on_FOV_area_exited(area: Area2D) -> void:
 	if area.is_in_group("PlayerArea"):
 		player_in_fov = false
+		if not isFixedCam and $RotationTimer.is_stopped():
+			$RotationTimer.start()
 	
 
 func _on_SureDetectionTimer_timeout() -> void:
 	Events.emit_signal("player_detected", Types.DetectionLevels.Sure)
 	Events.emit_signal("play_sound", "camera_alarm")
+
+	#now we recheck if player still in fov or he roun out, ofc with some delay
+	yield(get_tree().create_timer(2.0), "timeout")
+	var gareas = $FOV.get_overlapping_areas()
+	var player_still_in = false
+	for g in gareas:
+		if g.is_in_group("PlayerArea"):
+			player_still_in = true
+			
+	if not player_still_in:
+		set_state(Types.CameraStates.Normal)
+	else:
+		player_in_fov = true
+		print("back in fow")
+		set_state(Types.CameraStates.Suspect)
+		$RotationTimer.start()
 
 	
 func set_state(new_state) -> void:
@@ -60,12 +82,40 @@ func set_state(new_state) -> void:
 			Types.CameraStates.Normal:
 				if not $SureDetectionTimer.is_stopped():
 					$SureDetectionTimer.stop()
+				if "left" in $AnimationPlayer.assigned_animation:
+					animation("idle_left")
+				else:
+					animation("idle_right")
+					
+			Types.CameraStates.Suspect:
+				#print("Suspect mode")
+				print( $AnimationPlayer.assigned_animation )
+				
+				if not $SureDetectionTimer.is_stopped():
+					$SureDetectionTimer.stop()
+				if "left" in $AnimationPlayer.assigned_animation:
+					animation("susp_left")
+					$AnimationPlayer.seek(0,true)
+				else:
+					animation("susp_right")
+					$AnimationPlayer.seek(0,true)
+					
 			Types.CameraStates.PlayerDetected:
 				if $SureDetectionTimer.is_stopped():
 					$SureDetectionTimer.start()
+				if "left" in $AnimationPlayer.assigned_animation:
+					animation("detection_left")
+					$AnimationPlayer.seek(0,true)
+				else:
+					animation("detection_right")
+					$AnimationPlayer.seek(0,true)
+					
 				Events.emit_signal("player_detected", Types.DetectionLevels.Possible)
 				Events.emit_signal("play_sound", "camera_beep")
 				$AnimationPlayer.stop()
+				#reset rotation timer
+				if not isFixedCam:
+					$RotationTimer.stop()
 
 
 func animation(anim):
@@ -74,6 +124,7 @@ func animation(anim):
 
 
 func _on_RotationTimer_timeout():
+	atm_rotating = true
 	$FOV/CollisionPolygon2D.set_deferred("disabled", true)
 	
 	if currentDirection == CamDirectionType.Left:
@@ -97,6 +148,7 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 			$FOV.scale.x = -1
 
 		$FOV/CollisionPolygon2D.set_deferred("disabled", false)
+		atm_rotating = false
 		
 
 func deactivate() -> void:
