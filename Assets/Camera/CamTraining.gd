90182c62aa33a8aa06de882776eee1b2f580cdf3
kodkuce <kodkuce@mail.ru>
2020-11-25 01:16:10 +0100

    fix training camera

--- Assets/Camera/CamTraining.gd
+++ Assets/Camera/CamTraining.gd
@@ -9,13 +9,20 @@ func _ready():
 
 func _process(_delta: float) -> void:
 	if player_in_fov:
+		print( Global.player.visible_level )
 		match Global.player.visible_level:
 			Types.LightLevels.FullLight:
-				$AnimationPlayer.play("detected")
+				animation("detected")
 			Types.LightLevels.BarelyVisible:
-				$AnimationPlayer.play("suspect")
-			Types.LightLevels.Dark:
-				$AnimationPlayer.play("idle")
+				animation("suspect")
+			#Types.LightLevels.Dark:
+			#	animation("idle")
+				
+func animation(anim):
+	#print("anim: ", anim)
+	if $AnimationPlayer.current_animation != anim:
+		$AnimationPlayer.seek(0,true)
+		$AnimationPlayer.play(anim)
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
