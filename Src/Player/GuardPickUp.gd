d5bfde45f3f882fda6328e790884f4e8f94b8dec
Allen Davis <allendaviskoonan@gmail.com>
2020-12-12 14:37:27 +0530

    added untracked file xD

---
+++ Src/Player/GuardPickUp.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+export var rightOffset: Vector2
+export var leftOffset: Vector2
+var guard: Guard
+onready var player: Player = get_parent()
+
+func _ready() -> void:
+	set_process(false)
+	connect("body_entered", self, "onBodyEntered")
+	connect("body_exited", self, "onBodyExited")
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and guard.state == Types.GuardStates.Stunned and guard.isSleeping:
+		guard.set_state(Types.GuardStates.BeingDragged)
+		player.set_state(Types.PlayerStates.DraggingGuard)
+		return
+
+	if guard.state == Types.GuardStates.BeingDragged:
+		if player.direction.x > 0:
+			guard.global_position = player.global_position + rightOffset
+		elif player.direction.x < 0:
+			guard.global_position = player.global_position + leftOffset
+		if Input.is_action_just_pressed("interact"):
+			guard.state = Types.GuardStates.Stunned
+			player.set_state(Types.PlayerStates.Normal)
+
+
+func onBodyEntered(body: Node) -> void:
+	if body.is_in_group("Guard") and guard == null:
+		guard = body
+		set_process(true)
+
+
+func onBodyExited(body: Node) -> void:
+	if body == guard:
+		guard = null
+		set_process(false)
+
