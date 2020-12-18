f6a6b02b2b9221ad483af3295d142fddaf49704d
Allen Davis <allendaviskoonan@gmail.com>
2020-12-18 22:12:20 +0530

    progress made on elite guard

--- Src/Guard/EliteGuard/EliteGuard.gd
+++ Src/Guard/EliteGuard/EliteGuard.gd
@@ -1,2 +1,58 @@
 extends KinematicBody2D
 
+var direction: Vector2
+var velocity: Vector2
+var speed: int = 20
+var foundPlayer: bool
+var playerInLOS: bool
+var player
+
+onready var losRay: RayCast2D = $Flippable/LOSRay
+onready var pathLine: PathLine = $PathLine
+
+
+func _ready() -> void:
+    losRay.set_deferred("enabled", false)
+    $Flippable/LineOfSight.connect("body_entered", self, "onLOSBodyEntered")
+    $Flippable/TaserRange.connect("body_entered", self, "onTaserRangeBodyEntered")
+
+
+func _physics_process(delta: float) -> void:
+    velocity = direction * speed
+    velocity = move_and_slide(velocity)
+    updateFlip()
+    
+
+func _process(delta: float) -> void:
+    if playerInLOS:
+        if losRayIsCollidingWithPlayer():
+            foundPlayer = true
+            pathLine.moveToPoint(player.global_position)
+            Events.emit_signal("block_player_movement")
+            set_process(false)         
+
+    
+func onTaserRangeBodyEntered(body: Node) -> void:
+    if body.is_in_group("Player"):
+        player = body
+        $Flippable.scale.x = global_position.direction_to(body.global_position).x
+        print("start tasing player animation")
+        pathLine.stopAllMovement()
+        Events.emit_signal("game_over")
+
+
+func onLOSBodyEntered(body: Node) -> void:
+    if body.is_in_group("Player"):
+        losRay.set_deferred("enabled", true)
+        playerInLOS = true
+        player = body
+
+
+func losRayIsCollidingWithPlayer() -> bool:
+    return losRay.is_colliding() and losRay.get_collider().is_in_group("Player")
+    
+
+func updateFlip() -> void:
+    if direction.x != 0:
+        $Flippable.scale.x = direction.x
+    
