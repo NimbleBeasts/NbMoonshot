d6edbb704cfdbd62e950e7827bd0d79b672c23ea
Allen Davis <allendaviskoonan123@gmail.com>
2021-01-25 21:19:45 +0530

    fixed pressure button

--- Src/Button/PressureButton.gd
+++ Src/Button/PressureButton.gd
@@ -13,24 +13,21 @@ func _ready() -> void:
 func onAreaEntered(area: Area2D) -> void:
 	# don't need to put a check whether it collided with a pressure node or not 
 	# because this areas collision mask only allows it to collide with 'Pressure' objects
-	if pressureObjects == []:
-		# turned on
-		pressureObjects.append(area.mainNode)
-		area.mainNode.sprite.global_position.y -= offset
-		$Sprite.frame = 1
-		press()
+	# turned on
+	pressureObjects.append(area.mainNode)
+	$Sprite.frame = 1
+	press()
 
-
+	
 func onAreaExited(area: Area2D) -> void:
 	if pressureObjects.has(area.mainNode):
 		# turned off
 		pressureObjects.erase(area.mainNode)
-		area.mainNode.sprite.global_position.y += offset
-		$Sprite.frame = 0
 		if pressureObjects == []:
+			$Sprite.frame = 0
 			press()
 
-
+			
 func press() -> void:
 	for nodepath in unlocks:
 		var node = get_node(nodepath)
