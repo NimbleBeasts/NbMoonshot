814a1876cfe256aff0efd48c88f414533c939547
Allen Davis <allendaviskoonan123@gmail.com>
2021-01-25 21:36:42 +0530

    made the player not fall when standing on a ladder thin ceil

--- Src/Player/GroundDetection.gd
+++ Src/Player/GroundDetection.gd
@@ -10,22 +10,27 @@ func _ready():
 	connect("area_entered", self, "onAreaEntered")
 	connect("area_exited", self, "onAreaExited")
 
+
 func _process(delta):
 	if get_overlapping_bodies() == []:
 		emit_signal("apply_gravity")
 	set_process(false)
 
+
 func onBodyEntered(body: Node) -> void:
 	colliders.append(body)
+
 
 func onBodyExited(body: Node) -> void:
 	colliders.erase(body)
 	if colliders == []:
 		emit_signal("apply_gravity")
 
+
 func onAreaEntered(area: Area2D) -> void:
-	if area.is_in_group(areaCollidersGroup):
+	if area.is_in_group("AreaGround"):
 		colliders.append(area)
+
 
 func onAreaExited(area: Area2D) -> void:
 	if area in colliders:
