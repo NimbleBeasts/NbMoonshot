81cd5c5e751e2bfbc2bda72a1d9d185f2a3b1e64
Allen Davis <allendaviskoonan123@gmail.com>
2021-03-06 22:18:58 +0530

    Added reading csv translation files and updating nodes accordingly to the dialogue editor

--- addons/DialogueEditor/BaseBranch.gd
+++ addons/DialogueEditor/BaseBranch.gd
@@ -3,6 +3,7 @@ extends GraphNode
 class_name BaseBranch
 
 var quest: String = ""
+var id: String
 
 func _on_Branch_close_request():
 	queue_free()
@@ -13,3 +14,14 @@ func _on_Branch_resize_request(new_minsize):
 
 func has_only_one_connection() -> bool:
 	return get_connection_input_count() == 0
+
+
+func get_text_at_branch(index: int) -> String:
+	return get_node("LineEdit%s" % index).text
+
+func has_text_at_branch(index: int) -> bool:
+	return get_node("LineEdit%s" % index).text != ""
+	
+
+func get_text() -> String:
+	return get_node("Text").text
