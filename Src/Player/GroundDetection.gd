94ce8b4b7b9af6b259e45616b2c27acc9b039ffc
Allen Davis <allendaviskoonan123@gmail.com>
2021-03-25 14:04:19 +0530

    Made pickups collide with thin areas

--- Src/Player/GroundDetection.gd
+++ Src/Player/GroundDetection.gd
@@ -8,6 +8,13 @@ func _ready() -> void:
 	connect("body_entered", self, "onBodyEntered")
 	connect("body_exited", self, "onBodyExited")
 
+	# Setting collision layers to collide with thin areas
+	set_collision_layer_bit(0, true)
+	set_collision_mask_bit(0, true)
+	set_collision_mask_bit(2, true)
+	set_collision_mask_bit(11, true)
+
+
 func _process(delta):
 	if get_overlapping_bodies() == []:
 		emit_signal("apply_gravity")
@@ -22,4 +29,3 @@ func onBodyExited(body: Node) -> void:
 	colliders.erase(body)
 	if colliders == []:
 		emit_signal("apply_gravity")
-
