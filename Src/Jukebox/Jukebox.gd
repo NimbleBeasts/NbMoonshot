fc471053ac91fd40622166c05500b2dc48ad5e65
kodkuce <kodkuce@mail.ru>
2021-04-04 18:43:41 +0200

    lvl 9 base

---
+++ Src/Jukebox/Jukebox.gd
@@ -0,0 +1,55 @@
+extends Area2D
+
+export (Array, NodePath) var guards: Array
+
+var player
+var playingmusic = false
+
+func _ready() -> void:
+	connect("body_entered", self, "onBodyEntered")
+	connect("body_exited", self, "onBodyExited")
+	set_process_input(false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		press()
+
+func onBodyEntered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		player = body
+		set_process_input(true)
+
+func onBodyExited(body: Node) -> void:
+	if body.is_in_group("Player"):
+		player = null
+		set_process_input(false)
+
+func _on_Button_body_entered(body):
+	if body.is_in_group("Player"):
+		$Sprite.material.set_shader_param("active", true)
+
+
+func _on_Button_body_exited(body):
+	if body.is_in_group("Player"):
+		$Sprite.material.set_shader_param("active", false)
+
+
+func press():
+	if playingmusic:
+		return
+		
+	playingmusic = true
+	$SoundParticle.emitting = true
+	
+	for guardPath in guards:
+		var guard = get_node(guardPath)
+		if not guard.inDistractMode:
+			guard.distractMode()
+	
+	yield(get_tree().create_timer(10.0), "timeout")
+	playingmusic = false
+	$SoundParticle.emitting = false
---
+++ Src/Vault/VaultDoor.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+
+export var disabled_leaders : NodePath
+export var tresure_sack : NodePath
+var locked = true
+var player
+
+func open():
+	$AnimationPlayer.play("open")
+	get_node(disabled_leaders).disabled = true
+	locked = false
+
+
+
+
+func _ready() -> void:
+	connect("body_entered", self, "onBodyEntered")
+	connect("body_exited", self, "onBodyExited")
+	set_process_input(false)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		press()
+
+
+func onBodyEntered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		player = body
+		set_process_input(true)
+
+
+func onBodyExited(body: Node) -> void:
+	if body.is_in_group("Player"):
+		player = null
+		set_process_input(false)
+
+
+func press() -> void:
+	if locked:
+		return
+	var ts = get_node(tresure_sack)
+	if ts.global_position.y > 450:
+		ts.global_position = Vector2(400, 320)
+
+
+
+func _on_Button_body_entered(body):
+	if body.is_in_group("Player"):
+		$Sprite.material.set_shader_param("active", true)
+
+
+func _on_Button_body_exited(body):
+	if body.is_in_group("Player"):
+		$Sprite.material.set_shader_param("active", false)
