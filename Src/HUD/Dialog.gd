4ee9037384d4f4539441b5b806c44f1d31bc73d0
Allen Davis <allendaviskoonan123@gmail.com>
2021-04-09 15:29:18 +0530

    #11

--- Src/HUD/Dialog.gd
+++ Src/HUD/Dialog.gd
@@ -3,10 +3,16 @@ extends Control
 var dialogTyping: bool = false
 var currentOption: int = 0
 var noBranchFocusDelayTimer: Timer = Timer.new()
-
+var isOption: bool = false
+var holdTimer: Timer = Timer.new()
 
 func _ready() -> void:
+	set_process_input(false)
 	add_child(noBranchFocusDelayTimer)
+	add_child(holdTimer)
+	holdTimer.wait_time = 1
+	holdTimer.one_shot = true
+	holdTimer.connect("timeout", self, "skipDialog")
 	#warning-ignore:return_value_discarded
 	noBranchFocusDelayTimer.connect("timeout", self, "onNoBranchFocusDelayTimeout")
 	noBranchFocusDelayTimer.one_shot = true
@@ -14,16 +20,25 @@ func _ready() -> void:
 	Events.connect("update_no_branch_button_state", self, "changeNoBranchButtonState")
 	Events.connect("update_dialog_option", self, "onUpdateDialogOption")
 	#warning-ignore:return_value_discarded
-	$vbox/Option0Button.connect("button_up", self, "onOption0ButtonUp")
+	$vbox/Option0Button.connect("button_down", self, "onOption0ButtonUp")
 	#warning-ignore:return_value_discarded
-	$vbox/Option1Button.connect("button_up", self, "onOption1ButtonUp")
+	$vbox/Option1Button.connect("button_down", self, "onOption1ButtonUp")
 	#warning-ignore:return_value_discarded
-	$vbox/Option2Button.connect("button_up", self, "onOption2ButtonUp")
-	$vbox/NoBranchButton.connect("button_up", self, "onNoBranchButtonPressed")
+	$vbox/Option2Button.connect("button_down", self, "onOption2ButtonUp")
+	$vbox/NoBranchButton.connect("button_down", self, "onNoBranchButtonPressed")
 	Events.connect("dialog_typing_changed", self, "onDialogTypingChanged")
 	Events.connect("change_dialog_button_state", self, "changeButtonState")
 	changeNoBranchButtonState(false)
 	changeOptionButtonsState(false)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("interact"):
+		holdTimer.stop()
+		if isOption:
+			$vbox/Option0Button.grab_focus()
+		else:
+			$vbox/NoBranchButton.grab_focus()
 
 
 func onUpdateDialogOption(buttonType: int, newText: String) -> void:
@@ -74,7 +89,10 @@ func changeOptionButtonsState(enabled: bool) -> void:
 		i += 1
 	currentOption = 0
 	if enabled:
-		$vbox/Option0Button.grab_focus()
+		# $vbox/Option0Button.grab_focus()
+		set_process_input(true)
+		isOption = true
+		holdTimer.start()
 
 		
 func changeNoBranchButtonState(enabled: bool) -> void:
@@ -82,7 +100,10 @@ func changeNoBranchButtonState(enabled: bool) -> void:
 	$vbox/NoBranchButton.disabled = not enabled
 	if enabled:
 		# no idea why this needs a delay and the option ones doesn't 
-		noBranchFocusDelayTimer.start(0.08)
+		# noBranchFocusDelayTimer.start(0.08)
+		set_process_input(true)
+		isOption = false
+		holdTimer.start()
 
 
 func onDialogTypingChanged(value: bool) -> void:
