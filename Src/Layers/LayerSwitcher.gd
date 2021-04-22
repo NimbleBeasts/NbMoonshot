72e183edded444353c0eaf60c1dba4eec7f75722
kodkuce <kodkuce@mail.ru>
2021-04-22 23:26:34 +0200

    lvl13 update

--- Src/Layers/LayerSwitcher.gd
+++ Src/Layers/LayerSwitcher.gd
@@ -5,23 +5,26 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if(modulate.a < 1):
+		modulate.a += delta
+	else:
+		set_process(false)
 
 
 func _on_PlayerDetector_body_entered(body):
 	if body.is_in_group("Player"):
 		modulate.a = 0;
+		set_process(false)
 
 
 func _on_PlayerDetector_body_exited(body):
 	if body.is_in_group("Player"):
-		modulate.a = 1;
+		set_process(true)
 
